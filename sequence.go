065848b93e377954b3851bf32bec262608195303
Aron Nopanen <aron.nopanen@gmail.com>
2018-04-04 22:52:19 -0700

    Sequence runner unit tests
    Add unit tests for SequenceRunner, and fix bugs found by the unit tests.

--- sequence.go
+++ sequence.go
@@ -5,6 +5,8 @@ package animation
 
 import (
 	"image/color"
+	"log"
+	"os"
 	"time"
 )
 
@@ -20,8 +22,9 @@ const (
 type Step struct {
 	UniverseId     uint          // The universe to which the step is applied
 	Effect         Animation     // The animation effect to play
-	OnCompletionOf *Step         // Step that must complete before this step commences
+	OnCompletionOf int           // ID of step that must complete before this step commences
 	Delay          time.Duration // Delay before starting step [after prior step completes if set]
+	StepId         int           // ID of step, to allow reference by another step. Must be non-0 to be honored
 }
 
 type Sequence struct {
@@ -40,24 +43,25 @@ type stepAndTime struct {
 
 // Encapsulate a step waiting for another step to complete
 type stepAndGatingStep struct {
-	waitingOn *Step // Step waiting for completion
+	waitingOn int   // Step waiting for completion
 	toRun     *Step // Step to run
 }
 
 // SequenceRunner is responsible for executing a given sequence
 type SequenceRunner struct {
-	sequence         Sequence            // The sequence associated with this runner
 	awaitingTime     []stepAndTime       // Queue of steps waiting on a particular time
 	awaitingStep     []stepAndGatingStep // Queue of steps waiting on another step to complete
 	activeByUniverse [][]*Step           // Queue of steps that can be run on a particular universe. Only head of queue is procssed
 	buffers          [][]color.RGBA      // Buffers to hold universe data
 }
 
+var LOG *log.Logger = log.New(os.Stdout, "SEQUENCE", 0)
+
 // Create a SequenceRunner for the provided sequence with the specified universe
 // sizes. These size indicate the number of pixels in each universe, with the
 // universe ID being the index into the array. (Universe IDs are expected to
 // start at 0 and be consecutive.)
-func NewSequenceRunner(seq Sequence, universeSizes []uint) *SequenceRunner {
+func NewSequenceRunner(universeSizes []uint) *SequenceRunner {
 	// Create the framework of the SequenceRunner
 	activeByUniverse := make([][]*Step, len(universeSizes))
 	buffers := make([][]color.RGBA, len(universeSizes))
@@ -66,7 +70,6 @@ func NewSequenceRunner(seq Sequence, universeSizes []uint) *SequenceRunner {
 		buffers[idx] = make([]color.RGBA, size)
 	}
 	sr := &SequenceRunner{
-		seq,
 		make([]stepAndTime, DEFAULT_QUEUE_SIZE),
 		make([]stepAndGatingStep, DEFAULT_QUEUE_SIZE),
 		activeByUniverse,
@@ -75,9 +78,19 @@ func NewSequenceRunner(seq Sequence, universeSizes []uint) *SequenceRunner {
 	return sr
 }
 
-// Initialize the sequence associated with this SequenceRunner to start at the
-// provided time. If the sequence is already in process, it will be reinitialized.
-func (sr *SequenceRunner) InitSequence(now time.Time) {
+func findStep(steps []Step, stepId int) *Step {
+	for _, step := range steps {
+		if step.StepId == stepId {
+			return &step
+		}
+	}
+	return nil
+}
+
+// InitSequence initializes the SequenceRunner with the provides sequence, to
+// start at the provided time. If a sequence is already in process, it will be
+// stopped and the SequenceRunner reinitialized.
+func (sr *SequenceRunner) InitSequence(seq Sequence, now time.Time) {
 	// Clear structures
 	for idx := range sr.awaitingTime {
 		sr.awaitingTime[idx] = stepAndTime{}
@@ -95,13 +108,20 @@ func (sr *SequenceRunner) InitSequence(now time.Time) {
 	}
 
 	// Process the provided sequence steps
-	for _, step := range sr.sequence.Steps {
-		if step.OnCompletionOf != nil {
-			sr.awaitingStep = append(sr.awaitingStep, stepAndGatingStep{step.OnCompletionOf, &step})
+	for idx, step := range seq.Steps {
+		pstep := &seq.Steps[idx]
+		if step.OnCompletionOf != 0 {
+			waitingOnStep := findStep(seq.Steps, step.OnCompletionOf)
+			if waitingOnStep != nil {
+				sr.awaitingStep = append(sr.awaitingStep, stepAndGatingStep{waitingOnStep.StepId, pstep})
+			} else {
+				LOG.Printf("WARNING: Could not find step %d which step %v is waiting on. This step will be ignored",
+					step.OnCompletionOf, step)
+			}
 		} else if step.Delay > 0 {
-			sr.scheduleAt(&step, now.Add(step.Delay))
+			sr.scheduleAt(pstep, now.Add(step.Delay))
 		} else {
-			sr.activeByUniverse[step.UniverseId] = append(sr.activeByUniverse[step.UniverseId], &step)
+			sr.activeByUniverse[step.UniverseId] = append(sr.activeByUniverse[step.UniverseId], pstep)
 		}
 	}
 }
@@ -137,7 +157,7 @@ func (sr *SequenceRunner) handleStepComplete(completed *Step, now time.Time) {
 	}
 	for idx := 0; idx < len(sr.awaitingStep); {
 		waiting := sr.awaitingStep[idx]
-		if waiting.waitingOn == completed {
+		if waiting.waitingOn == completed.StepId {
 			s := waiting.toRun
 			if s.Delay > 0 {
 				// Schedule to run after delay
@@ -157,10 +177,10 @@ func (sr *SequenceRunner) handleStepComplete(completed *Step, now time.Time) {
 
 // Check for any tasks that should run at this point
 // 'now' is the time that should be considered to be the current time
-func (sr *SequenceRunner) checkScheuledTasks(now time.Time) {
+func (sr *SequenceRunner) checkScheduledTasks(now time.Time) {
 	for idx := 0; idx < len(sr.awaitingTime); {
 		waiting := sr.awaitingTime[idx]
-		if waiting.runAt.After(now) {
+		if now.After(waiting.runAt) {
 			// Time to run it!
 			s := waiting.toRun
 			sr.activeByUniverse[s.UniverseId] = append(sr.activeByUniverse[s.UniverseId], s)
@@ -177,14 +197,15 @@ func (sr *SequenceRunner) checkScheuledTasks(now time.Time) {
 // Return value indicates whether the sequence is complete.
 func (sr *SequenceRunner) ProcessFrame(now time.Time) (done bool) {
 	done = true
-	for UniverseId, universe := range sr.activeByUniverse {
+	sr.checkScheduledTasks(now)
+	for universeId, universe := range sr.activeByUniverse {
 		if len(universe) > 0 {
 			// We have an active step on this universe
 			s := universe[0]
 			// ...so we're not done yet
 			done = false
 			// Process the animation for the universe
-			effectDone := s.Effect.Frame(sr.buffers[UniverseId], now)
+			effectDone := s.Effect.Frame(sr.buffers[universeId], now)
 			if effectDone {
 				sr.handleStepComplete(s, now)
 			}
---
+++ sequence_test.go
@@ -0,0 +1,279 @@
+package animation
+
+import (
+	"image/color"
+	"testing"
+	"time"
+)
+
+type testAnimation uint8
+type multiRunAnimation int
+
+func (a *testAnimation) Frame(buf []color.RGBA, frameTime time.Time) bool {
+	//	fmt.Printf("In Frame for %v buf len %d, time %v\n", *a, len(buf), frameTime.Nanosecond()/1000)
+	count := uint8(*a)
+	for idx := range buf {
+		buf[idx] = color.RGBA{count, count + 1, count + 2, 0x00}
+		count += 3
+	}
+	return true
+}
+
+func (a *multiRunAnimation) Frame(buf []color.RGBA, frameTime time.Time) bool {
+	*a--
+	return *a <= 0
+}
+
+func TestDeleteStep(t *testing.T) {
+	s1 := Step{UniverseId: 1}
+	s2 := Step{UniverseId: 2}
+	s3 := Step{UniverseId: 3}
+	stepsArr := [3]*Step{&s1, &s2, &s3}
+	steps := stepsArr[:]
+	no0 := deleteStep(steps, 0)
+	if len(no0) != 2 || no0[0] != &s2 || no0[1] != &s3 {
+		t.Errorf("Delete 0 not as expected: %v", no0)
+	}
+	if stepsArr != [3]*Step{&s2, &s3, nil} {
+		t.Errorf("Underlying array not as expected after delete 0: %v", no0)
+	}
+}
+
+func TestSimpleSingleSequence(t *testing.T) {
+	t.SkipNow()
+	const universeId uint = 3
+	ta := testAnimation(1)
+	s := Step{UniverseId: universeId, Effect: &ta}
+	seq := Sequence{[]Step{s}}
+	sr := NewSequenceRunner([]uint{1, 1, 1, 10})
+	startTime := time.Unix(0, 0)
+	sr.InitSequence(seq, startTime)
+
+	// Check for right-sized empty buffer
+	buf := sr.UniverseData(universeId)
+	if len(buf) != 10 {
+		t.Errorf("Unexpected buffer size %d", len(buf))
+	}
+	zero := color.RGBA{0, 0, 0, 0}
+	for idx, p := range buf {
+		if p != zero {
+			t.Errorf("Pixel %d not 0 (%x)", idx, p)
+		}
+	}
+
+	// Call the effect to generate data
+	done := sr.ProcessFrame(startTime)
+	if done {
+		t.Error("Done on first frame")
+	}
+
+	// Check for updated data
+	count := uint8(1)
+	buf = sr.UniverseData(universeId)
+	if len(buf) != 10 {
+		t.Errorf("Unexpected buffer size %d", len(buf))
+	}
+	for idx, p := range buf {
+		expected := color.RGBA{count, count + 1, count + 2, 0x00}
+		if p != expected {
+			t.Errorf("Pixel %d value (%v) not expected (%v)", idx, p, expected)
+		}
+		count += 3
+	}
+
+	done = sr.ProcessFrame(startTime.Add(time.Millisecond))
+	if !done {
+		t.Error("Not done on second call to ProcessFrame")
+	}
+}
+
+func TestTwoSimpleSequences(t *testing.T) {
+	ta1 := testAnimation(1)
+	s1 := Step{UniverseId: 3, Effect: &ta1}
+	ta2 := testAnimation(31)
+	s2 := Step{UniverseId: 1, Effect: &ta2}
+	seq := Sequence{[]Step{s1, s2}}
+	sr := NewSequenceRunner([]uint{1, 6, 1, 10})
+	startTime := time.Unix(0, 0)
+	sr.InitSequence(seq, startTime)
+
+	// Check for right-sized empty buffer
+	buf := sr.UniverseData(1)
+	if len(buf) != 6 {
+		t.Errorf("Unexpected buffer size %d", len(buf))
+	}
+	zero := color.RGBA{0, 0, 0, 0}
+	for idx, p := range buf {
+		if p != zero {
+			t.Errorf("Pixel %d not 0 (%x)", idx, p)
+		}
+	}
+	buf = sr.UniverseData(3)
+	if len(buf) != 10 {
+		t.Errorf("Unexpected buffer size %d", len(buf))
+	}
+	for idx, p := range buf {
+		if p != zero {
+			t.Errorf("Pixel %d not 0 (%x)", idx, p)
+		}
+	}
+
+	// Call the effect to generate data
+	done := sr.ProcessFrame(startTime)
+	if done {
+		t.Error("Done on first frame")
+	}
+
+	// Check for updated data
+	buf = sr.UniverseData(1)
+	count := uint8(31)
+	for idx, p := range buf {
+		expected := color.RGBA{count, count + 1, count + 2, 0x00}
+		if p != expected {
+			t.Errorf("Pixel %d value (%v) not expected (%v)", idx, p, expected)
+		}
+		count += 3
+	}
+	buf = sr.UniverseData(3)
+	count = uint8(1)
+	for idx, p := range buf {
+		expected := color.RGBA{count, count + 1, count + 2, 0x00}
+		if p != expected {
+			t.Errorf("Pixel %d value (%v) not expected (%v)", idx, p, expected)
+		}
+		count += 3
+	}
+
+	done = sr.ProcessFrame(startTime.Add(time.Millisecond))
+	if !done {
+		t.Error("Not done on second call to ProcessFrame")
+	}
+}
+
+func TestMultiRun(t *testing.T) {
+	ta1 := multiRunAnimation(3)
+	s1 := Step{UniverseId: 3, Effect: &ta1}
+	ta2 := multiRunAnimation(1)
+	s2 := Step{UniverseId: 0, Effect: &ta2}
+	seq := Sequence{[]Step{s1, s2}}
+	sr := NewSequenceRunner([]uint{1, 1, 1, 1})
+	now := time.Unix(0, 0)
+	sr.InitSequence(seq, now)
+
+	if sr.ProcessFrame(now) {
+		t.Error("Done on call 1")
+	}
+	if sr.ProcessFrame(now.Add(time.Millisecond)) {
+		t.Error("Done on call 2")
+	}
+	if sr.ProcessFrame(now.Add(2 * time.Millisecond)) {
+		t.Error("Done on call 3")
+	}
+	if !sr.ProcessFrame(now.Add(3 * time.Millisecond)) {
+		t.Error("Not done on call 4")
+	}
+}
+
+func TestDelay(t *testing.T) {
+	ta1 := testAnimation(1)
+	s1 := Step{UniverseId: 3, Effect: &ta1, Delay: 9 * time.Millisecond}
+	ta2 := testAnimation(31)
+	s2 := Step{UniverseId: 1, Effect: &ta2}
+	seq := Sequence{[]Step{s1, s2}}
+	sr := NewSequenceRunner([]uint{1, 6, 1, 10})
+	now := time.Unix(0, 0)
+	sr.InitSequence(seq, now)
+
+	for tick := 0; tick < 10; tick++ {
+		frameTime := now.Add(time.Duration(tick) * time.Millisecond)
+		if sr.ProcessFrame(frameTime) {
+			t.Errorf("Done on call for tick %d, time %v", tick, frameTime)
+		}
+		if sr.UniverseData(3)[0].R != 0 {
+			t.Error("Delayed effect ran too early")
+		}
+		if sr.UniverseData(1)[0].R == 0 {
+			t.Error("Immediate effect didn't run")
+		}
+	}
+
+	if sr.ProcessFrame(now.Add(10 * time.Millisecond)) {
+		t.Error("Done at time 10")
+	}
+	if sr.UniverseData(3)[0].R == 0 {
+		t.Error("Delayed effect didn't run at right time")
+	}
+
+	if !sr.ProcessFrame(now.Add(11 * time.Millisecond)) {
+		t.Error("Not done at time 11")
+	}
+}
+
+func TestScheduleAfter(t *testing.T) {
+	ta1 := testAnimation(1)
+	ta2 := testAnimation(31)
+	s2 := Step{UniverseId: 3, Effect: &ta2, StepId: 2}
+	s1 := Step{UniverseId: 1, Effect: &ta1, OnCompletionOf: 2}
+	seq := Sequence{[]Step{s1, s2}}
+	sr := NewSequenceRunner([]uint{1, 1, 1, 1})
+	now := time.Unix(0, 0)
+	sr.InitSequence(seq, now)
+
+	for tick := 0; tick < 1; tick++ {
+		frameTime := now.Add(time.Duration(tick) * time.Millisecond)
+		if sr.ProcessFrame(frameTime) {
+			t.Errorf("Done on call for tick %d, time %v", tick, frameTime)
+		}
+		if sr.UniverseData(1)[0].R != 0 {
+			t.Error("Contingent effect ran too early")
+		}
+		if sr.UniverseData(3)[0].R == 0 {
+			t.Error("Immediate effect didn't run")
+		}
+	}
+
+	if sr.ProcessFrame(now.Add(1 * time.Millisecond)) {
+		t.Error("Done at time 1")
+	}
+	if sr.UniverseData(1)[0].R == 0 {
+		t.Error("Contingent effect didn't run at right time")
+	}
+
+	if !sr.ProcessFrame(now.Add(2 * time.Millisecond)) {
+		t.Error("Not done at time 2")
+	}
+}
+
+func TestScheduleAfterPlusDelay(t *testing.T) {
+	ta1 := testAnimation(1)
+	ta2 := testAnimation(31)
+	s2 := Step{UniverseId: 3, Effect: &ta2, StepId: 2}
+	s1 := Step{UniverseId: 1, Effect: &ta1, OnCompletionOf: 2, Delay: time.Millisecond * 2}
+	seq := Sequence{[]Step{s1, s2}}
+	sr := NewSequenceRunner([]uint{1, 1, 1, 1})
+	now := time.Unix(0, 0)
+	sr.InitSequence(seq, now)
+
+	for tick := 0; tick < 3; tick++ {
+		frameTime := now.Add(time.Duration(tick) * time.Millisecond)
+		if sr.ProcessFrame(frameTime) {
+			t.Errorf("Done on call for tick %d, time %v", tick, frameTime)
+		}
+		if sr.UniverseData(1)[0].R != 0 {
+			t.Error("Contingent effect ran too early")
+		}
+		if sr.UniverseData(3)[0].R == 0 {
+			t.Error("Immediate effect didn't run")
+		}
+	}
+	if sr.ProcessFrame(now.Add(3 * time.Millisecond)) {
+		t.Error("Done at time 3")
+	}
+	if sr.UniverseData(1)[0].R == 0 {
+		t.Error("Contingent effect didn't run at right time")
+	}
+
+	if !sr.ProcessFrame(now.Add(4 * time.Millisecond)) {
+		t.Error("Not done at time 4")
+	}
+}
